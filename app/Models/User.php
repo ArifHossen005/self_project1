1ad325cc6c175660535eef2502f0137ec8798d1c
Md Arif Hossen <arifsohag2500@gmail.com>
2024-11-20 21:42:29 +0600

    install laravel breeze

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];  //its means all fillable fields are allowed
 
     /**
      * The attributes that should be hidden for serialization.
